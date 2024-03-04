Fix the bug in converting Euler angles to a rotation matrix and add point cloud projection functionality

scene/cameras.py
@@ -15,8 +15,7 @@ import numpy as np
 from utils.graphics_utils import getWorld2View2, getProjectionMatrix
 
 class Camera(nn.Module):
-    #def __init__(self, colmap_id, R, T, FoVx, FoVy, image, gt_alpha_mask,
-    def __init__(self, colmap_id, R, T, FoVx, FoVy, cx, cy, image, gt_alpha_mask,
+    def __init__(self, colmap_id, R, T, FoVx, FoVy, cx, cy,fx,fy,image, gt_alpha_mask,
                  image_name, uid,
                  trans=np.array([0.0, 0.0, 0.0]), scale=1.0, data_device = "cuda"
                  ):
@@ -28,6 +27,8 @@ class Camera(nn.Module):
         self.T = T
         self.FoVx = FoVx
         self.FoVy = FoVy
+        self.fx = fx
+        self.fy = fy
         self.cx = cx
         self.cy = cy
         self.image_name = image_name
@@ -56,9 +57,9 @@ class Camera(nn.Module):
         self.scale = scale
 
         self.world_view_transform = torch.tensor(getWorld2View2(R, T, trans, scale)).transpose(0, 1).cuda()
-        #self.projection_matrix = getProjectionMatrix(znear=self.znear, zfar=self.zfar, fovX=self.FoVx, fovY=self.FoVy).transpose(0,1).cuda()
         self.projection_matrix = getProjectionMatrix(znear=self.znear, zfar=self.zfar,
                                                      fovX=self.FoVx, fovY=self.FoVy,
+                                                     fx = self.fx  ,fy = self.fy,
                                                      cx=self.cx, cy=self.cy).transpose(0, 1).cuda()
         self.full_proj_transform = (self.world_view_transform.unsqueeze(0).bmm(self.projection_matrix.unsqueeze(0))).squeeze(0)
         self.camera_center = self.world_view_transform.inverse()[3, :3]

scene/colmap_loader.py
@@ -244,55 +244,25 @@ def read_intrinsics_binary(path_to_model_file):
     return cameras
 
 def pose_rotation(pose):
-
     euler_angles = [pose["yaw"], pose["pitch"], pose["roll"]]
-    rotation = Rotation.from_euler('zyx', euler_angles)
+    rotation = Rotation.from_euler('ZYX', euler_angles)
 
     return rotation
 
 def pose_translation(pose):
-
     translation = np.array([pose["x"], pose["y"], pose["z"]])
 
     return translation
 
 def create_transform_matrix(rotation_matrix, translation_vector):
-    """
-    创建变换矩阵
-
-    参数：
-    - rotation_matrix: 3x3 的旋转矩阵
-    - translation_vector: 3 维的位移向量
-
-    返回：
-    - transform_matrix: 4x4 的变换矩阵
-    """
-    # 创建一个单位矩阵作为变换矩阵的基础
     transform_matrix = np.eye(4)
-
-    # 将旋转矩阵的内容复制到变换矩阵的左上角（3x3 子矩阵）
     transform_matrix[:3, :3] = rotation_matrix
-
-    # 将位移向量复制到变换矩阵的右侧（前三个元素）
     transform_matrix[:3, 3] = translation_vector
 
     return transform_matrix
 
 def decompose_transform_matrix(transform_matrix):
-    """
-    将变换矩阵分离为旋转矩阵和位移向量
-
-    参数：
-    - transform_matrix: 4x4 的变换矩阵
-
-    返回：
-    - rotation_matrix: 3x3 的旋转矩阵
-    - translation_vector: 3 维的位移向量
-    """
-    # 提取旋转矩阵（左上角的3x3子矩阵）
     rotation_matrix = transform_matrix[:3, :3]
-
-    # 提取位移向量（右侧的前三个元素）
     translation_vector = transform_matrix[:3, 3]
 
     return rotation_matrix, translation_vector
@@ -302,6 +272,16 @@ def pose_to_transform(pose):
     translation = pose_translation(pose)
     transform = create_transform_matrix(rotation,translation)
     return transform
+
+def transform_inv(transform):
+    rotation=transform[:3,:3]
+    rotation=rotation.T
+    translation=transform[:3,3]
+    translation=-rotation@translation
+    inv_transform=create_transform_matrix(rotation,translation)
+    return inv_transform
+
+
 
 def read_intrinsics_Json(path_to_model_file):
     """
@@ -313,54 +293,37 @@ def read_intrinsics_Json(path_to_model_file):
     # 假设文件路径为 path_to_json_file
     with open(path_to_model_file, 'r') as json_file: 
         data = json.load(json_file)
-        # 获取 run_params 列表中的第一个元素
         run_params = data["run_params"][0]
-        # 遍历 camera_params 列表
         for camera_params in run_params["camera_params"]:
-            # 获取 camera参数
             camera_id = camera_params["camera_id"]
             width = camera_params["width"]
             height = camera_params["height"]
             model = "PINHOLE"
             camera_matrix = camera_params["camera_matrix"]
-            #Colmap的内参是： fy": 686.5524553956232, "fx": 826.1663097380575
             params = [camera_matrix["fx"],camera_matrix["fy"],camera_matrix["cx"],camera_matrix["cy"]]
-            #params = [826.1663097380575,686.5524553956232,camera_matrix["cx"],camera_matrix["cy"]]
-            
-            
-            # 在这里可以使用 camera_id 进行其他操作 686.552455395623  
-            '''
-            print("Camera ID:", camera_id)
-            print("Camera width:", width)
-            print("Camera height:", height)
-            print("Camera model:", model)
-            print("Camera params:", params)
-            '''
-            #if(camera_id == "CAM_PBQ_FRONT_RIGHT" or camera_id == "CAM_PBQ_FRONT_LEFT" ):
-            if(1):
+           
+            if(camera_id == "CAM_PBQ_FRONT_LEFT"):
                 cameras[camera_id] = Camera(id=camera_id, model=model,
                                             width=width, height=height,
                                             params=params)
     return cameras
 
 def save_ply(points, filename):
-    vertices = np.array(points, dtype=[('x', 'f4'), ('y', 'f4'), ('z', 'f4')])
-    plydata = PlyData([PlyElement.describe(vertices, 'vertex')])
-    plydata.write(filename)
+    try:
+        vertices = np.array(points, dtype=[('x', 'f4'), ('y', 'f4'), ('z', 'f4')])
+        plydata = PlyData([PlyElement.describe(vertices, 'vertex')])
+        plydata.write(filename)
+    except Exception as e:
+        print(f"Error occurred while writing to the PLY file{e}")
 
 def read_extrinsics_Json(path_to_model_file):
-     # 读取 JSON 数据
     with open(path_to_model_file, 'r') as json_file: 
         data = json.load(json_file)
         images = {}
-        # 获取第一个轨迹
         trajectory = data["trajectories"][0]
-        # 在循环之前初始化计数器
         image_id = 0
-        # 初始化一个列表，用于存储所有tvec
         all_tvec_points = []
 
-        # 使用字典保存 camera_id 对应的 camera_to_vehicle
         camera_to_vehicle_pose = {}
         for run_param in data["run_params"]:
             for camera_param in run_param["camera_params"]:
@@ -368,64 +331,41 @@ def read_extrinsics_Json(path_to_model_file):
                     camera_to_vehicle = camera_param["camera_to_vehicle"]
                     camera_to_vehicle_pose[camera_id] = camera_to_vehicle
 
-            # 打印保存的字典
-            #print(camera_to_vehicle_pose['CAM_PBQ_FRONT_TELE']['x'])
-            # 遍历轨迹中的所有视图节点
         for view_node in trajectory["view_nodes"]:
-            #timestamp = view_node["timestamp"]
             timestamp = '{:.3f}'.format(view_node["timestamp"])
-
-            # 获取与之相关的相机对齐信息
             align_images_info = view_node["align_images_info"]
             vehicle_pose =  view_node["pose"]
             for align_info in align_images_info:
                 camera_id = align_info["camera_id"]
                 camera_pose = align_info["pose"]
-                
-                RR = np.array([ [ 0,  0, 1],
-                                [-1,  0, 0],
-                                [ 0, -1, 0]])
-                # 计算变换
+                RR = np.array([ [ 0,  -1, 0],
+                                [0,  0, -1],
+                                [ 1, 0, 0]])           
+
                 transform_vehicle_to_world = pose_to_transform(vehicle_pose)
-                transform_camera_align = pose_to_transform(camera_pose)
+                transform_camera_align = transform_inv(pose_to_transform(camera_pose))
                 transform_camera_to_vehicle = pose_to_transform(camera_to_vehicle_pose[camera_id])
-
-                transform_vehicle_to_world_plus = np.dot(transform_vehicle_to_world,transform_camera_align)
-                transform_camera_to_world = np.dot(transform_vehicle_to_world_plus,transform_camera_to_vehicle)
-                transform_camera_to_world[:3, :3] = np.dot(transform_camera_to_world[:3, :3],RR)
-                transform_world_to_camera = np.linalg.inv(transform_camera_to_world)
-                #print("inverse_matrix",inverse_matrix)
-
-
+                transform_camera_to_world = np.dot(transform_vehicle_to_world,np.dot(transform_camera_align,transform_camera_to_vehicle))
+              
+ 
+                transform_world_to_camera = transform_inv(transform_camera_to_world)
+                transform_test = transform_inv(transform_camera_to_world)
+                transform_world_to_camera[:3, :3] = np.dot(RR,transform_world_to_camera[:3, :3])
+                transform_world_to_camera[:3, 3] = np.dot(RR,transform_world_to_camera[:3, 3])
+ 
                 rotation_world_to_camera , translation_world_to_camera = decompose_transform_matrix(transform_world_to_camera)
-                #print("translation_world_to_camera",translation_world_to_camera)
-
-                
-                #rotation_world_to_camera = np.dot(np.linalg.inv(RR),rotation_world_to_camera)
-                qvec = Rotation.from_matrix(rotation_world_to_camera).as_quat()
-                #print("q1",qvec)
                 qvec = rotmat2qvec(rotation_world_to_camera)
-                #print("q2",qvec)
                 tvec = translation_world_to_camera
                 
                 image_name = "/" +str(timestamp) + "/" + camera_id + ".png"
-                #if(camera_id == "CAM_PBQ_FRONT_LEFT"  and image_id<=1000000):
-                #if(camera_id == "CAM_PBQ_FRONT_RIGHT" or camera_id == "CAM_PBQ_FRONT_LEFT" ):
-                if(image_id<=1000000):
+                if(camera_id == "CAM_PBQ_FRONT_LEFT"):
                     images[image_id] = Image(
                         id=image_id, qvec=qvec, tvec=tvec,
                         camera_id=camera_id, name=image_name,
                         xys=None, point3D_ids=None)
-                    #print("Camera camera_id:", camera_id)
-                    #r1 , t1 = decompose_transform_matrix(transform_camera_to_world)
-                    #print(tvec)
-                    # 将当前tvec添加到列表中
                     all_tvec_points.append(tuple(tvec))
-                    #print("rotation_world_to_camera",rotation_world_to_camera)
-                    # 每次循环后递增计数器
                     image_id += 1
-    # 保存所有tvec到PLY文件
-    save_ply(all_tvec_points, 'output_point_cloud.ply')
+    save_ply(all_tvec_points, 'home/qcraft/gaussian_splatting/output_point_cloud.ply')
     return images
 def read_extrinsics_text(path):
     """

scene/dataset_readers.py
@@ -31,6 +31,8 @@ class CameraInfo(NamedTuple):
     FovX: np.array
     cx: np.array
     cy: np.array
+    fx: np.array
+    fy: np.array
     image: np.array
     image_path: str
     image_name: str
@@ -82,9 +84,7 @@ def readColmapCameras(cam_extrinsics, cam_intrinsics, images_folder):
 
         uid = intr.id
         R = np.transpose(qvec2rotmat(extr.qvec))
-        #print(R.T)
         T = np.array(extr.tvec)
-        #print(T)
         if intr.model=="SIMPLE_PINHOLE":
             focal_length_x = intr.params[0]
             FovY = focal2fov(focal_length_x, height)
@@ -92,28 +92,20 @@ def readColmapCameras(cam_extrinsics, cam_intrinsics, images_folder):
         elif intr.model=="PINHOLE":
             focal_length_x = intr.params[0]
             focal_length_y = intr.params[1]
-            cx = intr.params[2]
-            cy = intr.params[3]
+            fx = intr.params[0] / width
+            fy = intr.params[1] / height
+            cx = intr.params[2] / width
+            cy = intr.params[3] / height 
             FovY = focal2fov(focal_length_y, height)
             FovX = focal2fov(focal_length_x, width)
         else:
             assert False, "Colmap camera model not handled: only undistorted datasets (PINHOLE or SIMPLE_PINHOLE cameras) supported!"
 
-        cx = (cx - width / 2) / width * 2
-        cy = (cy - height / 2) / height * 2
         image_path = images_folder + extr.name
-        #print(image_path)
-        '''
-        print("Camera images_folder:", images_folder)
-        print("Camera name:", extr.name)
-        print("Camera image_path:", image_path)
-        '''
         image_name = os.path.basename(image_path).split(".")[0]
         image = Image.open(image_path)
 
-        #cam_info = CameraInfo(uid=uid, R=R, T=T, FovY=FovY, FovX=FovX, image=image,
-        #                      image_path=image_path, image_name=image_name, width=width, height=height)
-        cam_info = CameraInfo(uid=uid, R=R, T=T, FovY=FovY, FovX=FovX, cx=cx, cy=cy, image=image,
+        cam_info = CameraInfo(uid=uid, R=R, T=T, FovY=FovY, FovX=FovX, cx=cx, cy=cy, fx=fx,fy=fy,image=image,
                               image_path=image_path, image_name=image_name, width=width, height=height)
         cam_infos.append(cam_info)
     sys.stdout.write('\n')

train.py
@@ -34,6 +34,45 @@ try:
 except ImportError:
     TENSORBOARD_FOUND = False
 
+def save_image_local(ori_image,save_path):
+    cv_image=cv2.cvtColor(ori_image.permute(1,2,0).cpu().detach().numpy()*255.0,cv2.COLOR_RGB2BGR)
+    cv2.imwrite(save_path,cv_image)
+
+def render_python(viewpoint_cam, gaussians):
+    height = viewpoint_cam.image_height
+    width = viewpoint_cam.image_width
+    Fx = viewpoint_cam.Fx * width
+    Fy = viewpoint_cam.Fy * height
+    Cx = viewpoint_cam.cx * width
+    Cy = viewpoint_cam.cy * height
+    R, t = viewpoint_cam.R, viewpoint_cam.T
+
+    # 构建变换矩阵
+    TR = np.eye(4)
+    TR[:3, :3] = R.T
+    TR[:3, 3] = t
+
+
+    image = torch.zeros(height, width, 3, dtype=torch.float32)
+
+    for point in gaussians._xyz:
+        point_cpu = point.detach().cpu().numpy()
+        p_hom = TR[:3, :3]@point_cpu+ TR[:3, 3]
+        
+        if p_hom[2] < 0 :
+            continue
+
+        if(p_hom[2]==0):
+            p_hom[2]=0.000000001
+
+        p_proj = [p_hom[0] * Fx / p_hom[2] + Cx,
+                  p_hom[1] * Fy / p_hom[2] + Cy]
+        if 0 <= p_proj[0] < width and 0 <= p_proj[1] < height:
+            image[int(p_proj[1]), int(p_proj[0]), :] = 1.0
+
+    image = image.permute(2, 0, 1)
+    return image
+
 def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoint_iterations, checkpoint, debug_from):
     first_iter = 0
     tb_writer = prepare_output_and_logger(dataset)
@@ -91,79 +130,59 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
 
         render_pkg = render(viewpoint_cam, gaussians, pipe, bg)
         image, viewspace_point_tensor, visibility_filter, radii = render_pkg["render"], render_pkg["viewspace_points"], render_pkg["visibility_filter"], render_pkg["radii"]
-        if iteration % 2000 == 0:
-            # 获取全部的 get_xyz 张量
-            xyz_tensor = gaussians.get_xyz
-
-            # 将张量转换为 NumPy 数组
-            xyz_array = xyz_tensor.cpu().detach().numpy()
-
-            if os.path.exists('xyz_coordinates.txt'):
-                os.remove('xyz_coordinates.txt')
-                #print("remove over")
-            # 保存到文本文件
-            np.savetxt('xyz_coordinates.txt', xyz_array, fmt='%f')
         
-        # 1:  模式1 将mask以外的值赋0
-        # 2:  模式2 将mask以外的值删除
-        # 3:  模式3 不做mask
-        mask_mode=1
+      
+        # Loss
+        gt_image = viewpoint_cam.original_image.cuda()
+        height, width = gt_image.size(1), gt_image.size(2)
 
-        if mask_mode == 1:
-        # 模式1：将掩码以外的值赋为0
-             # Loss
-            gt_image = viewpoint_cam.original_image.cuda()
-            # 创建与 gt_image 大小相同的掩码张量
-            height, width = gt_image.size(1), gt_image.size(2)
+        mask = torch.zeros((3, height, width), device=gt_image.device)
 
-            # 创建与 gt_image 大小相同的掩码张量
-            mask = torch.zeros((3, height, width), device=gt_image.device)
-
-            # 设置某些像素的值为 1
-            mask[0:500, 0:1600] = 1
-        
-            # 逐元素相乘
+        if (viewpoint_cam.colmap_id == "CAM_PBQ_FRONT_WIDE" ):
+            mask[:, 0:2*height//3, 0:width] = 1
+            image *=mask
+            gt_image *=mask 
+        elif (viewpoint_cam.colmap_id == "CAM_PBQ_REAR"):
+            mask[:, 0:4*height//5, 0:width] = 1
             image *=mask
             gt_image *=mask
-            
-            Ll2 = l2_loss(image, gt_image)
-            loss = (1.0 - opt.lambda_dssim) * Ll2 + opt.lambda_dssim * (1.0 - ssim(image, gt_image))
-            loss.backward()
-        elif mask_mode == 2:
-            # 模式2：将掩码以外的值删除
-            # Loss
-            gt_image = viewpoint_cam.original_image.cuda()
-            # 创建与 gt_image 大小相同的掩码张量，获取 gt_image 的大小
-            height, width = gt_image.size(1), gt_image.size(2)
+        elif (viewpoint_cam.colmap_id == "CAM_PBQ_REAR_LEFT"):
+            pass
+            mask[:, 0:height, 1*width//3:width] = 1
+            image *=mask
+            gt_image *=mask 
+        elif (viewpoint_cam.colmap_id == "CAM_PBQ_REAR_RIGHT"):
+            mask[:, 0:height, 0:2*width//3] = 1
+            image *=mask
+            gt_image *=mask
+        elif (viewpoint_cam.colmap_id == "CAM_PBQ_FRONT_FISHEYE"):
+            mask[:, 2*height//3:height, 0:width] = 1
+            image *=mask
+            gt_image *=mask
+        elif (viewpoint_cam.colmap_id == "CAM_PBQ_LEFT_FISHEYE"):
+            mask[:, 2*height//3:height, 0:width] = 1
+            image *=mask
+            gt_image *=mask
+        elif (viewpoint_cam.colmap_id == "CAM_PBQ_REAR_FISHEYE"):
+            mask[:, 2*height//3:height, 0:width] = 1
+            image *=mask
+            gt_image *=mask
+        elif (viewpoint_cam.colmap_id == "CAM_PBQ_RIGHT_FISHEYE"):
+            mask[:, 2*height//3:height, 0:width] = 1
+            image *=mask
+            gt_image *=mask
 
-            # 创建与 gt_image 大小相同的掩码张量
-            mask = torch.zeros((3, height, width), device=gt_image.device)
-
-            # 设置某些像素的值为 1
-            mask[0:500, 0:1600] = 1
-            # 创建一个掩码，选择值不等于0的元素
-            nonzero_mask = mask != 0
-
-            # 使用掩码选择非零元素
-            image_nonzero = torch.masked_select(image, nonzero_mask)
-            gt_image_nonzero = torch.masked_select(gt_image, nonzero_mask)
-
-            # 重新构造图像形状
-            image_nonzero = image_nonzero.view_as(image)
-            gt_image_nonzero = gt_image_nonzero.view_as(gt_image)
-
-            # 使用新的非零图像进行计算
-            Ll2 = l2_loss(image_nonzero, gt_image_nonzero)
-            loss = (1.0 - opt.lambda_dssim) * Ll2 + opt.lambda_dssim * (1.0 - ssim(image_nonzero, gt_image_nonzero))
-            loss.backward()
-        elif mask_mode == 3:
-            # Loss
-            gt_image = viewpoint_cam.original_image.cuda()
-            
-            Ll2 = l2_loss(image, gt_image)
-            loss = (1.0 - opt.lambda_dssim) * Ll2 + opt.lambda_dssim * (1.0 - ssim(image, gt_image))
-            loss.backward()
-
+        save_flag = 0             
+        if(save_flag==1):    
+            test_image=render_python(viewpoint_cam, gaussians)
+            save_image_local(test_image,"/home/qcraft/Gaussian_Splatting_WS/fix_pro/test.jpg")
+            save_image_local(gt_image,"/home/qcraft/Gaussian_Splatting_WS/fix_pro/gt.jpg")
+            save_image_local(image,"/home/qcraft//Gaussian_Splatting_WS/fix_pro/pred.jpg")
+            import sys
+            sys.exit()
+        Ll2 = l2_loss(image, gt_image)
+        loss = (1.0 - opt.lambda_dssim) * Ll2 + opt.lambda_dssim * (1.0 - ssim(image, gt_image))
+        loss.backward()
         iter_end.record()
 
         with torch.no_grad():

utils/camera_utils.py
@@ -23,14 +23,14 @@ def loadCam(args, id, cam_info, resolution_scale):
     orig_w, orig_h = cam_info.image.size
 
     if args.resolution in [1, 2, 4, 8]:
-        resolution = round(orig_w/(resolution_scale * args.resolution)), round(orig_h/(resolution_scale * args.resolution))
+        resolution = round(orig_w / (resolution_scale * args.resolution)), round(orig_h / (resolution_scale * args.resolution))
     else:  # should be a type that converts to float
         if args.resolution == -1:
             if orig_w > 1600:
                 global WARNED
                 if not WARNED:
                     print("[ INFO ] Encountered quite large input images (>1.6K pixels width), rescaling to 1.6K.\n "
-                        "If this is not desired, please explicitly specify '--resolution/-r' as 1")
+                          "If this is not desired, please explicitly specify '--resolution/-r' as 1")
                     WARNED = True
                 global_down = orig_w / 1600
             else:
@@ -42,21 +42,17 @@ def loadCam(args, id, cam_info, resolution_scale):
         resolution = (int(orig_w / scale), int(orig_h / scale))
 
     resized_image_rgb = PILtoTorch(cam_info.image, resolution)
-    # 创建保存目录 gt（如果目录不存在）
+    # Create the save directory 'gt' (if it doesn't exist)
     save_directory = 'gt'
     os.makedirs(save_directory, exist_ok=True)
 
-    # 获取 gt 目录下已有文件的数量，用于确定新文件的编号
+    # Get the number of existing files in the 'gt' directory to determine the new file's number
     existing_files = [f for f in os.listdir(save_directory) if os.path.isfile(os.path.join(save_directory, f))]
     file_number = len(existing_files) + 1
 
-    # 构建保存文件的完整路径，命名方式为 saved_image_1.jpg、saved_image_2.jpg，依此类推
+    # Build the full path for the saved file, naming convention is saved_image_1.jpg, saved_image_2.jpg, and so on
     save_path = os.path.join(save_directory, f"saved_image_{file_number}.jpg")
 
-    # 假设 cam_info.image 是一个 PIL Image 对象
-    #cam_info.image.save(save_path)
-
-    #print(f"Saved to: {save_path}")
 
     gt_image = resized_image_rgb[:3, ...]
     loaded_mask = None
@@ -64,8 +60,8 @@ def loadCam(args, id, cam_info, resolution_scale):
     if resized_image_rgb.shape[1] == 4:
         loaded_mask = resized_image_rgb[3:4, ...]
 
-    return Camera(colmap_id=cam_info.uid, R=cam_info.R, T=cam_info.T, 
-                  #FoVx=cam_info.FovX, FoVy=cam_info.FovY, 
+    return Camera(colmap_id=cam_info.uid, R=cam_info.R, T=cam_info.T,
+                  fx=cam_info.fx, fy=cam_info.fy,
                   FoVx=cam_info.FovX, FoVy=cam_info.FovY,
                   cx=cam_info.cx, cy=cam_info.cy,
                   image=gt_image, gt_alpha_mask=loaded_mask,
@@ -79,28 +75,24 @@ def cameraList_from_camInfos(cam_infos, resolution_scale, args):
 
     return camera_list
 
-def camera_to_JSON(id, camera : Camera):
+def camera_to_JSON(id, camera: Camera):
     Rt = np.zeros((4, 4))
     Rt = np.eye(4)
     Rt[:3, :3] = camera.R.T
     Rt[:3, 3] = camera.T
-    RR = np.array([  [0, 0, 1],
-                        [-1, 0, 0],
-                        [0, -1, 0]])
 
     W2C = np.linalg.inv(Rt)
-    pos = W2C[:3, 3]
-    pos = np.dot(-camera.R,camera.T)
+    pos = np.dot(-camera.R, camera.T)
     rot = W2C[:3, :3]
     serializable_array_2d = [x.tolist() for x in rot]
     camera_entry = {
-        'id' : id,
-        'img_name' : camera.image_name,
-        'width' : camera.width,
-        'height' : camera.height,
+        'id': id,
+        'img_name': camera.image_name,
+        'width': camera.width,
+        'height': camera.height,
         'position': pos.tolist(),
         'rotation': serializable_array_2d,
-        'fy' : fov2focal(camera.FovY, camera.height),
-        'fx' : fov2focal(camera.FovX, camera.width)
+        'fy': fov2focal(camera.FovY, camera.height),
+        'fx': fov2focal(camera.FovX, camera.width)
     }
     return camera_entry

utils/graphics_utils.py
@@ -49,29 +49,16 @@ def getWorld2View2(R, t, translate=np.array([.0, .0, .0]), scale=1.0):
 
     return np.float32(Rt)
 
-#def getProjectionMatrix(znear, zfar, fovX, fovY):
-def getProjectionMatrix(znear, zfar, fovX, fovY, cx, cy):
-    tanHalfFovY = math.tan((fovY / 2))
-    tanHalfFovX = math.tan((fovX / 2))
-
-    top = tanHalfFovY * znear
-    bottom = -top
-    right = tanHalfFovX * znear
-    left = -right
-
+def getProjectionMatrix(znear, zfar, fovX, fovY, fx, fy, cx, cy):
+    
     P = torch.zeros(4, 4)
 
-    z_sign = 1.0
-
-    P[0, 0] = 2.0 * znear / (right - left)
-    P[1, 1] = 2.0 * znear / (top - bottom)
-    P[0, 2] = cx
-    P[1, 2] = cy
-    P[0, 2] = (right + left) / (right - left)
-    P[1, 2] = (top + bottom) / (top - bottom)
-    P[3, 2] = z_sign
-    P[2, 2] = z_sign * zfar / (zfar - znear)
-    P[2, 3] = -2 * (zfar * znear) / (zfar - znear)
+    P[0, 0] = 2*fx
+    P[1, 1] = 2*fy
+    P[0, 2] = 2*cx - 1
+    P[1, 2] = 2*cy - 1
+    P[2, 2] = 1
+    P[3, 2] = 1
     return P
 
 def fov2focal(fov, pixels):